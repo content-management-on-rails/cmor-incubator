bbb34bc83773c2a10156bbfe22208eb1adcde634
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-11-26 20:29:14 +0100

    feat(Cmor::TimeTracking): show issues in the project show view

--- cmor-time_tracking/app/models/cmor/time_tracking/issue.rb
+++ cmor-time_tracking/app/models/cmor/time_tracking/issue.rb
@@ -17,5 +17,29 @@ module Cmor::TimeTracking
     def items_count
       items.count
     end
+
+    def items_hours
+      items.sum(:duration).to_f / 60 / 60
+    end
+
+    def items_draft_hours
+      items.draft.sum(:duration).to_f / 60 / 60
+    end
+
+    def items_confirmed_hours
+      items.confirmed.sum(:duration).to_f / 60 / 60
+    end
+
+    def items_billable_hours
+      items.billable.sum(:duration).to_f / 60 / 60
+    end
+
+    def items_not_billable_hours
+      items.not_billable.sum(:duration).to_f / 60 / 60
+    end
+
+    def items_billed_hours
+      items.billed.sum(:duration).to_f / 60 / 60
+    end
   end
 end
