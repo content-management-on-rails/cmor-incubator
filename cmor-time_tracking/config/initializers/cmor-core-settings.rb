5931da6776087d72fd0e0979d69f3a6e94afaca5
Roberto Vasquez-Angel <rva@beegoodit.de>
2023-12-12 16:09:50 +0100

    fix(Cmor::TimeTracking): rails not starting due to settings being created instead of registered

--- cmor-time_tracking/config/initializers/cmor-core-settings.rb
+++ cmor-time_tracking/config/initializers/cmor-core-settings.rb
@@ -1,31 +1,34 @@
 Rails.application.config.to_prepare do
-  Cmor::Core::Settings::Setting.create!(
-    namespace: :cmor_time_tracking,
-    key: "external_issue.jira.integration_enabled",
-    type: :boolean,
-    default: ENV.fetch("CMOR_TIME_TRACKING_EXTERNAL_ISSUE_JIRA_INTEGRATION_ENABLED", false),
-    validations: {inclusion: {in: [true, false]}}
-  )
-  Cmor::Core::Settings::Setting.create!(
-    namespace: :cmor_time_tracking,
-    key: "external_issue.jira.base_url",
-    type: :string,
-    default: ENV.fetch("CMOR_TIME_TRACKING_EXTERNAL_ISSUE_JIRA_BASE_URL", nil)
-  )
+  Cmor::Core::Settings.configure do |config|
+    config.register(
+      namespace: :cmor_time_tracking,
+      key: "external_issue.jira.integration_enabled",
+      type: :boolean,
+      default: ENV.fetch("CMOR_TIME_TRACKING_EXTERNAL_ISSUE_JIRA_INTEGRATION_ENABLED", false),
+      validations: {inclusion: {in: [true, false]}}
+    )
+    
+    config.register(
+      namespace: :cmor_time_tracking,
+      key: "external_issue.jira.base_url",
+      type: :string,
+      default: ENV.fetch("CMOR_TIME_TRACKING_EXTERNAL_ISSUE_JIRA_BASE_URL", nil)
+    )
 
-  Cmor::Core::Settings::Setting.create!(
-    namespace: :cmor_time_tracking,
-    key: "external_issue.jira.username",
-    type: :string,
-    default: ENV.fetch("CMOR_TIME_TRACKING_EXTERNAL_ISSUE_JIRA_USERNAME", nil),
-    validations: {presence: true}
-  )
+    config.register(
+      namespace: :cmor_time_tracking,
+      key: "external_issue.jira.username",
+      type: :string,
+      default: ENV.fetch("CMOR_TIME_TRACKING_EXTERNAL_ISSUE_JIRA_USERNAME", nil),
+      validations: {presence: true}
+    )
 
-  Cmor::Core::Settings::Setting.create!(
-    namespace: :cmor_time_tracking,
-    key: "external_issue.jira.api_token",
-    type: :string,
-    default: ENV.fetch("CMOR_TIME_TRACKING_EXTERNAL_ISSUE_JIRA_API_TOKEN", nil),
-    validations: {presence: true}
-  )
+    config.register(
+      namespace: :cmor_time_tracking,
+      key: "external_issue.jira.api_token",
+      type: :string,
+      default: ENV.fetch("CMOR_TIME_TRACKING_EXTERNAL_ISSUE_JIRA_API_TOKEN", nil),
+      validations: {presence: true}
+    )
+  end
 end
